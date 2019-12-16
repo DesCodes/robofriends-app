243c85d042444fc4451e7f23497c52bc685a221f
Desmond <desmond.yan.lai@gmail.com>
2019-12-16 15:48:54 -0500

    Updates

---
+++ precache-manifest.21c1aeaef99ba39ee7b1cb85aaf7e8a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d830ae38a6ac22c4dcf2265f095023",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/robofriends-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "06b102584b289286bdc8",
+    "url": "/robofriends-app/static/css/main.7512684d.chunk.css"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/robofriends-app/static/js/2.ee428d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends-app/static/js/2.ee428d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06b102584b289286bdc8",
+    "url": "/robofriends-app/static/js/main.637a44ab.chunk.js"
+  },
+  {
+    "revision": "8d6bb2cd24fed03d5da7",
+    "url": "/robofriends-app/static/js/runtime-main.d1653d3b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
